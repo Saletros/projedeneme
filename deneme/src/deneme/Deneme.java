6d6f2235adfc0a03570e46de30c648632ea452b7
Furkan <Furkan@192.168.157.1>
2021-06-19 22:17:07 +0300

    2

--- deneme/src/deneme/Deneme.java
+++ deneme/src/deneme/Deneme.java
@@ -5,6 +5,7 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("deneme");
         System.out.println("deneme2");
+        System.out.println("deneme3");
     }
     
 }
