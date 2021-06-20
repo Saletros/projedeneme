cb77d30ca5c7ce1e1d0786b02b79b1f0e461214c
HP İ5 <HP 5@192.168.1.24>
2021-06-20 13:50:20 +0300

    deneme 5 eklendi

--- deneme/src/deneme/Deneme.java
+++ deneme/src/deneme/Deneme.java
@@ -7,6 +7,7 @@ public class Deneme {
         System.out.println("deneme2");
         System.out.println("deneme3");
         System.out.println("deneme4");
+        System.out.println("deneme5");
     }
     
 }
